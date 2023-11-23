35294a3b93aa84b0ab7cb53a2cc92fb12ffe87d9
André Lashley <andre.lashley@gmail.com>
2023-11-23 14:19:19 -0800

    Add database config

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
+from flask import jsonify
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/users')
+def users():
+    # return jsonify({'pdfs': pdf_list})
+    return jsonify({'username': 'andre'})
