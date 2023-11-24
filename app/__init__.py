610dcd4f069f1b88d540d9332f0df266ba690cf5
André Lashley <andre.lashley@gmail.com>
2023-11-23 16:54:38 -0800

    Add email

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,13 @@ import logging
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
 import os
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 from app import routes, models, errors
 
